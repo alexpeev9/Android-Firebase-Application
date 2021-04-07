fa04536f5d67c966d8e0e94a36b108b006bce1de
alexpeev9 <62713012+alexpeev9@users.noreply.github.com>
2021-04-07 11:55:59 +0300

    Added Working Authentication 100%
    Login and Register
    fixed xml typo

--- app/src/main/java/com/example/simplefirebaseapp/LoginActivity.java
+++ app/src/main/java/com/example/simplefirebaseapp/LoginActivity.java
@@ -1,32 +1,81 @@
 package com.example.simplefirebaseapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
 
-    @BindView(R.id.edt_email) EditText editEmail;
+    @BindView(R.id.edt_email) EditText edtEmail;
 
-    @BindView(R.id.edt_password)  EditText editPassword;
-
+    @BindView(R.id.edt_password)  EditText edtPassword;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+        mAuth = FirebaseAuth.getInstance();
+
     }
 
     @OnClick(R.id.btn_login)
     public void onLoginClicked(){
+        String email = edtEmail.getText().toString();
+        String password = edtPassword.getText().toString();
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this, "Authentication successful!.",
+                                    Toast.LENGTH_SHORT).show();
+                            redirectToMainScreen();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+                }
 
-    }
     @OnClick(R.id.btn_register)
     public void onRegisterClicked(){
-
+        String email = edtEmail.getText().toString();
+        String password = edtPassword.getText().toString();
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this, "Authentication successful!.",
+                                    Toast.LENGTH_SHORT).show();
+                            redirectToMainScreen();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+    public void redirectToMainScreen(){
+        startActivity(new Intent(this,MainActivity.class));
+        finish(); // we don't want to go back to login while logged
     }
 }
